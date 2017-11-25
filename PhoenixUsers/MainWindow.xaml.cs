3c49d4bd0c452a6ec6c1bb791e69e61ba9652d2b
Yavor Georgiev <jimbolp@gmail.com>
2017-11-25 11:52:47 +0200

    Updated List with branches from the database

--- PhoenixUsers/MainWindow.xaml.cs
+++ PhoenixUsers/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.Entity.Core;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,60 @@ namespace PhoenixUsers
     /// </summary>
     public partial class MainWindow : Window
     {
+        UserDBEntities db;
+        ObservableCollection<Branch> branches;
+        ObservableCollection<Position> positions;
+        ObservableCollection<UserMasterData> users;
         public MainWindow()
         {
-            InitializeComponent();
+            try
+            {
+                Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+                db = new UserDBEntities();
+                InitializeComponent();
+                InitializeBranchListBox();
+                InitializePositionsListBox();
+                InitializeDataGrid();
+            }
+            catch (EntityException)
+            {
+                MessageBox.Show("There was a problem connecting to the database!");
+                Application.Current.Shutdown(-1);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Application.Current.Shutdown(-1);
+            }
+        }
+
+        private void InitializeDataGrid()
+        {
+            string sql = "select * from UserMasterData";
+            users = new ObservableCollection<UserMasterData>(db.Database.SqlQuery<UserMasterData>(sql).ToList());
+            
+            UsersTable.ItemsSource = users;
+        }
+
+        private void InitializePositionsListBox()
+        {
+
+        }
+
+        private void InitializeBranchListBox()
+        {
+            string sql = "select * from Branch";
+            branches = new ObservableCollection<Branch>();
+            var br = from branch in db.Database.SqlQuery<Branch>(sql)
+                     select branch;
+            branches.Clear();
+            foreach (Branch b in br)
+            {
+                branches.Add(b);
+            }
+            //ListBranches.DataContext = branches;
+            ListBranches.ItemsSource = from branch in branches
+                                       select branch.BranchName;
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
---
+++ PhoenixUsers/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixUsers
+{
+    class User
+    {
+        [DisplayName("№")]
+        public int ID { get; set; }
+        [DisplayName("Потребител")]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        [DisplayName("Активна Директория")]
+        public string ActiveDirectory { get; set; }
+        [DisplayName("Длъжност")]
+        public string Position { get; set; }
+        [DisplayName("Склад")]
+        public string Depo { get; set; }
+        [DisplayName("Фармос Акаунт")]
+        public string PharmosUserName { get; set; }
+        [DisplayName("Акаунт в UADM")]
+        public string UADMUserName { get; set; }
+        [DisplayName("GoodsIn")]
+        public string GoodsIn { get; set; }
+        [DisplayName("Purchase")]
+        public string PurchaseAccount { get; set; }
+        [DisplayName("Tender")]
+        public string TenderAccount { get; set; }
+        [DisplayName("Фибра")]
+        public string PhibraAccount { get; set; }
+        [DisplayName("KSC акаунт")]
+        public string KSCAccount { get; set; }
+        [DisplayName("Статус")]
+        public string State { get; set; }
+        [DisplayName("Доп. Информация")]
+        public string Description { get; set; }
+    }
+}
